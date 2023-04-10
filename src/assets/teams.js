d846ebfe2647efebcaa85cd96a7a94c3623260f5
Shalaginov Ivan <shalaginov.dev@gmail.com>
2023-04-10 22:28:45 +0400

    add new fonts

--- src/assets/teams.js
+++ src/assets/teams.js
@@ -4,30 +4,30 @@ import TeamSecret from "./img/teamLogo/TeamSecret.png";
 import TeamSpirit from "./img/teamLogo/TeamSpirit.png";
 
 export const teams = [
-    {
-        id: '1',
-        image: TeamLiquid,
-        title: 'TeamLiquid',
-        players: [
-            {id: '152962063', nickname: 'miCKe'},
-            {id: 'liquid2', nickname: 'Nisha'},
-            {id: '73562326', nickname: 'zai'},
-            {id: '77490514', nickname: 'Boxi'},
-            {id: '54580962', nickname: 'Insania'},
-        ]
-    },
-    {
-        id: '2',
-        image: OG,
-        title: 'OG',
-        players: [
-            {id: 'og1', nickname: 'Yuragi'},
-            {id: 'og2', nickname: 'bzm'},
-            {id: 'og3', nickname: 'DM'},
-            {id: 'og4', nickname: 'Taiga'},
-            {id: 'og5', nickname: 'Chu'},
-        ]
-    },
+    // {
+    //     id: '1',
+    //     image: TeamLiquid,
+    //     title: 'TeamLiquid',
+    //     players: [
+    //         {id: '152962063', nickname: 'miCKe'},
+    //         {id: 'liquid2', nickname: 'Nisha'},
+    //         {id: '73562326', nickname: 'zai'},
+    //         {id: '77490514', nickname: 'Boxi'},
+    //         {id: '54580962', nickname: 'Insania'},
+    //     ]
+    // },
+    // {
+    //     id: '2',
+    //     image: OG,
+    //     title: 'OG',
+    //     players: [
+    //         {id: 'og1', nickname: 'Yuragi'},
+    //         {id: 'og2', nickname: 'bzm'},
+    //         {id: 'og3', nickname: 'DM'},
+    //         {id: 'og4', nickname: 'Taiga'},
+    //         {id: 'og5', nickname: 'Chu'},
+    //     ]
+    // },
     {
         id: '3',
         image: TeamSecret,
@@ -40,18 +40,18 @@ export const teams = [
             {id: '87278757', nickname: 'Puppey'},
         ]
     },
-    {
-        id: '4',
-        image: TeamSpirit,
-        title: 'TeamSpirit',
-        players: [
-            {id: 'spirit1', nickname: 'Yatoro'},
-            {id: 'spirit2', nickname: 'Larl'},
-            {id: 'spirit3', nickname: 'Collapse'},
-            {id: 'spirit4', nickname: 'Mira'},
-            {id: 'spirit5', nickname: 'Miposhka'},
-        ]
-    },
+    // {
+    //     id: '4',
+    //     image: TeamSpirit,
+    //     title: 'TeamSpirit',
+    //     players: [
+    //         {id: 'spirit1', nickname: 'Yatoro'},
+    //         {id: 'spirit2', nickname: 'Larl'},
+    //         {id: 'spirit3', nickname: 'Collapse'},
+    //         {id: 'spirit4', nickname: 'Mira'},
+    //         {id: 'spirit5', nickname: 'Miposhka'},
+    //     ]
+    // },
 ]
 
 
